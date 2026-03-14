d079876a952eb3a476b8d2d75135d72e49accd4a
siham2006el-cpu <siham2006el@gmail.com>
2026-03-14 09:59:10 +0100

    clean data

---
+++ src/data_processing.py
@@ -0,0 +1,27 @@
+# (Data Understanding)
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+# charger le fichier
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), '..', 'data', 'ObesityDataSet_raw_and_data_sinthetic.csv'))
+
+# afficher les premières lignes
+print(df.head())
+
+# vérifier les valeurs manquantes
+print(df.isnull().sum())
+
+# Vérifier les statistiques générales
+df.describe()
+
+# Vérifier les doublons
+df.duplicated().sum()
+
+# supprimer les lignes dupliquées
+df = df.drop_duplicates()
+
+# réinitialiser l'index
+df = df.reset_index(drop=True)
