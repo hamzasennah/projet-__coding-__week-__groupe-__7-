d4e80b8a75ef7c19890e42ed56e81965435f7aa2
zouhairimad <zohairimade@gmail.com>
2026-03-14 15:25:32 +0000

    hhhhhh

--- src/train_model.py
+++ src/train_model.py
@@ -1,22 +1,31 @@
+
 import pandas as pd
-import os 
+import joblib
+import os
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
+
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 
 
 # ===============================
-# Load dataset
+# 1 Load dataset
 # ===============================
-
 
 data_path = os.path.join(os.path.dirname(__file__), '..', 'data' , 'data_clean.csv')
 
 df = pd.read_csv(data_path)
+
+print("Dataset loaded")
+print("Columns:", df.columns)
+
+
+# ===============================
+# 2 Target (last column)
+# ===============================
 
 target = df.columns[-1]
 
@@ -25,7 +34,7 @@ y = df[target]
 
 
 # ===============================
-# Train Test Split
+# 3 Train Test Split
 # ===============================
 
 X_train, X_test, y_train, y_test = train_test_split(
@@ -37,7 +46,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 
 # ===============================
-# Models
+# 4 Models
 # ===============================
 
 models = {
@@ -47,28 +56,43 @@ models = {
 }
 
 
+best_model = None
+best_score = 0
+best_name = ""
+
+
 # ===============================
-# Evaluation
+# 5 Training models
 # ===============================
 
 for name, model in models.items():
 
-    print("\n===========================")
-    print("Model:", name)
+    print("\nTraining:", name)
 
     model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
     y_proba = model.predict_proba(X_test)
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average="weighted")
-    recall = recall_score(y_test, y_pred, average="weighted")
-    f1 = f1_score(y_test, y_pred, average="weighted")
-    roc_auc = roc_auc_score(y_test, y_proba, multi_class="ovr")
+    roc_auc = roc_auc_score(
+        y_test,
+        y_proba,
+        multi_class="ovr"
+    )
 
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("F1-score:", f1)
     print("ROC-AUC:", roc_auc)
+
+    if roc_auc > best_score:
+        best_score = roc_auc
+        best_model = model
+        best_name = name
+
+
+# ===============================
+# 6 Save best model
+# ===============================
+
+joblib.dump(best_model, "best_model.pkl")
+
+print("\nBest model:", best_name)
+print("Best ROC-AUC:", best_score)
+print("Model saved as best_model.pkl")
